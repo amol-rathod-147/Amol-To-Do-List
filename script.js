8388678bdbb928de594ecd8ba529c3f761baa347
amol_first <amolamolamol147@gmail.com>
2024-11-30 14:01:45 +0530

    Stored input task, time, date in the variable

---
+++ script.js
@@ -0,0 +1,7 @@
+const addTaskButton=document.getElementById("addTaskButton");
+addTaskButton.addEventListener("click",()=>{
+    const task=document.getElementById("taskInput").value.trim();
+    const date=document.getElementById("taskDate").value;
+    const time=document.getElementById("taskTime").value;
+    
+})
